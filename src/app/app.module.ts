093149c4e61eb990ae984d3e41ba64d5cb6a10c4
Jacob Schulzetenberg <jacob.schulzetenberg@gmail.com>
2018-02-25 11:22:55 -0700

    Angular material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-
+import { MaterialCustomModule } from './material-custom/material-custom.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialCustomModule
   ],
   providers: [],
   bootstrap: [AppComponent]
